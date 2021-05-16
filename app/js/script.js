88dff1670ca7b0e4c3bf4cd222aa2fa88d9bcbbe
kipscan <g.gergely92@gmail.com>
2021-05-16 18:53:54 +0200

    workaround for mobile background resize issue

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,5 @@
-const intro = document.querySelector('.intro');
 
+const intro = document.querySelector('.intro');
 // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
 let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
